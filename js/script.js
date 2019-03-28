775b1077f01b0bebf4fe68d87f6376c5500b9da9
Xurxe <xurxe.garcia@integrify.io>
2019-03-28 15:19:14 +0200

    Improved syles

--- js/script.js
+++ js/script.js
@@ -2,44 +2,86 @@
 
 const countriesWrapper = document.querySelector('.countries-wrapper');
 
-const subtitle = document.querySelector('.subtitle');
+const countryCount = document.querySelector('.country-count');
 
 const searchInput = document.querySelector('.search-input');
 
-const buttons = document.querySelector('.buttons');
+const buttonsDiv = document.querySelector('.buttons-div');
+
+const h1 = document.querySelector('h1');
+
+const a = document.querySelector('a');
 
 let alphaByName = true;
 let alphaByCapital = false;
 let descendingByPopulation = false;
 
+let H = Math.round(Math.random() * 360);
+let S = Math.round(Math.random() * 100);
+let mediumL = 25;
+let mediumHSL = `hsl(${H}, ${S}%, ${mediumL}%)`
 
+let darkL = 5;
+let darkHSL = `hsl(${H}, ${S}%, ${darkL}%)`
 
+/* STYLES ***************************************************************** */
+searchInput.style.background = mediumHSL;
+searchInput.style.border = `2px solid ${mediumHSL}`;
 
-/* FUNCTIONS *************************************************************** */
-const showCountries = (array) => {
-    countryDivInnerHtml = '';
-    countriesWrapper.innerHTML = '';
+searchInput.addEventListener('mouseover', function() {
+    searchInput.style.background = 'white';
+});
 
-    array.forEach((country, i) => {
-        countryDivInnerHtml += createCountryDivInnerHtml(country);
+searchInput.addEventListener('mouseout', function() {
+    searchInput.style.background = mediumHSL;
+});
+
+searchInput.addEventListener('focus', function() {
+    searchInput.style.background = 'white';
+});
+
+searchInput.addEventListener('blur', function() {
+    searchInput.style.background = mediumHSL;
+});
+
+const buttons = document.querySelectorAll('button')
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.padding = '1rem';
+    buttons[i].style.margin = '1rem 0';
+    buttons[i].style.background = 'white';
+    buttons[i].style.border = `2px solid ${mediumHSL}`;
+    buttons[i].style.width = '9rem';
+    buttons[i].style.fontSize = '0.8rem';
+    buttons[i].style.color = mediumHSL;
+
+    buttons[i].addEventListener('mouseover', function() {
+        buttons[i].style.color = 'white';
+        buttons[i].style.background = mediumHSL;
     });
-
-    countriesWrapper.innerHTML = countryDivInnerHtml;
-
+    
+    buttons[i].addEventListener('mouseout', function() {
+        buttons[i].style.color = mediumHSL;
+        buttons[i].style.background = 'white';
+    });
+    
+    buttons[i].addEventListener('focus', function() {
+        buttons[i].style.color = 'white';
+        buttons[i].style.background = mediumHSL;
+    });
+    
+    buttons[i].addEventListener('blur', function() {
+        buttons[i].style.color = mediumHSL;
+        buttons[i].style.background = 'white';
+    });
 };
 
 
 
-buttons.addEventListener('click', e => {
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.className);
-});
 
-
-
-const createCountryDivInnerHtml = object => {
+/* FUNCTIONS *************************************************************** */
+function createCountryDivInnerHtml(object) {
     const {name, capital, languages, population, flag} = object;
+
     countryDivInnerHtml =
     `<div class="country-div">
         <div class="flag-div">
@@ -52,22 +94,49 @@ const createCountryDivInnerHtml = object => {
             <p class="population-p"><span class="property-name">Population:</span> ${population.toLocaleString()}</p>
             <p class="languages-p"><span class="property-name">Languages:</span> ${languages.join(', ')}</p>
         </div>
-
     </div>`
+
     return countryDivInnerHtml;
+};
+
+function generateCountryDivColor() {
+    const countryDivs = document.querySelectorAll('.country-div');
+    for (i = 0; i < countryDivs.length; i++) {
+        let L = Math.round(Math.random() * (90 - 60) + 60);
+        let hsl = `hsl(${H}, ${S}%, ${L}%)`;
+
+        countryDivs[i].style.background = hsl;
+        countryDivs[i].style.color = darkHSL;
+    }
+}
+
+
+
+function showCountries(array) {
+    countryDivInnerHtml = '';
+    countriesWrapper.innerHTML = '';
+
+    for (i = 0; i < array.length; i++) {
+        countryDivInnerHtml += createCountryDivInnerHtml(array[i]);
+    };
+
+    countriesWrapper.innerHTML = countryDivInnerHtml;
+
+    generateCountryDivColor();
+
 };
 
 
 
-searchInput.addEventListener('input', e => {
-    countriesWrapper.innerHTML = '';
-    let searchTerm = e.target.value.toLowerCase();
-    showCountries(filterCountries(countries, searchTerm));
-});
+function changeSearchMode(event) {
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.className);
+};
 
 
 
-const filterCountries = (array, search) => {
+function filterCountries(array, search) {
     const filteredCountries = array.filter(country => {
         let {name, capital, languages} = country;
         let isName = name.toLowerCase().includes(search);
@@ -82,7 +151,22 @@ const filterCountries = (array, search) => {
 
 
 
+function showFilteredCountries(event) {
+    countriesWrapper.innerHTML = '';
+    let searchTerm = event.target.value.toLowerCase();
+    shownCountries = showCountries(filterCountries(countries, searchTerm));
+};
+
+
+
 /* EXECUTION *************************************************************** */
 
-subtitle.textContent = `showing ${countries.length} countries`;
+h1.style.color = mediumHSL;
+a.style.color = mediumHSL;
+
+countryCount.textContent = `showing ${countries.length} countries`;
 showCountries(countries);
+
+buttonsDiv.addEventListener('click', changeSearchMode);
+
+searchInput.addEventListener('input', showFilteredCountries);
